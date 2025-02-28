700b23815bb88da336bdcb717bbfeea34ea9cb8e
candrepa1 <candrepa1@gmail.com>
2025-02-28 18:59:20 -0500

    fix: remove profile from routing

--- src/components/Routing/Routing.tsx
+++ src/components/Routing/Routing.tsx
@@ -5,7 +5,6 @@ import Entries from "../../pages/Entries";
 import Dashboard from "../../pages/Dashboard";
 import Trends from "../../pages/Trends";
 import Insights from "../../pages/Insights";
-import Profile from "../../pages/Profile";
 
 const Routing = () => {
   return (
@@ -16,7 +15,6 @@ const Routing = () => {
           <Route path="entries" element={<Entries />} />
           <Route path="trends" element={<Trends />} />
           <Route path="insights" element={<Insights />} />
-          <Route path="profile" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
